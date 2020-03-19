273fbb5368ec15d9fd431eb20e232c838a6a7434
Klology <brandenm64@yahoo.com>
2020-03-19 11:13:34 -0500

    Created the basic SongLibraryData class

---
+++ Jukebox Heroes/Jukebox Heros/SongLibrary/SongLibraryData.cs
@@ -0,0 +1,21 @@
+﻿using Jukebox_Heros.Song;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jukebox_Heros.SongLibrary
+{
+    class SongLibraryData
+    {
+        List<SongData> songData = new List<SongData>();
+
+        public SongLibraryData() {
+
+        }
+
+
+
+    }
+}
