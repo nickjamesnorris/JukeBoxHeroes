424b962bf11ceeef5cf9a0138a3090e7c67f3f2b
Klology <brandenm64@yahoo.com>
2020-03-19 11:28:54 -0500

    Added thew Newtonsoft Json library so we can easily serialize and deserialize json

--- Jukebox Heroes/Jukebox Heros/SongLibrary/SongLibraryData.cs
+++ Jukebox Heroes/Jukebox Heros/SongLibrary/SongLibraryData.cs
@@ -1,21 +1,30 @@
 ﻿using Jukebox_Heros.Song;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Jukebox_Heros.SongLibrary
 {
     class SongLibraryData
     {
-        List<SongData> songData = new List<SongData>();
+        private const string libraryFilePath = "./data/library.json";
+        List<SongData> songList = new List<SongData>();
 
         public SongLibraryData() {
 
         }
 
+        public void saveLibrary() {
+            File.WriteAllText(libraryFilePath, JsonConvert.SerializeObject(songList));
+        }
 
+        public void loadLibrary() {
+            songList = JsonConvert.DeserializeObject<List<SongData>>(File.ReadAllText(libraryFilePath));
+        }
 
     }
 }
