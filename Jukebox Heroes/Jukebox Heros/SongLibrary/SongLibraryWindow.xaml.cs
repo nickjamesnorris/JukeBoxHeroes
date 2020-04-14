5cdc83c317df5364bc5c547d3e610a761ce806e0
Klology <brandenm64@yahoo.com>
2020-04-14 02:05:07 -0500

    Made it so you can now sort the library by clicking on the column headers

--- Jukebox Heroes/Jukebox Heros/SongLibrary/SongLibraryWindow.xaml.cs
+++ Jukebox Heroes/Jukebox Heros/SongLibrary/SongLibraryWindow.xaml.cs
@@ -22,6 +22,10 @@ namespace Jukebox_Heroes.SongLibrary
         ISongLibraryData songLibrary;
         ISongUpload songUpload;
         IPlaylistData playlist;
+
+        GridViewColumnHeader _lastHeaderClicked = null;
+        ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
         public ObservableCollection<SongData> songList {
             get;
             set;
@@ -57,21 +61,55 @@ namespace Jukebox_Heroes.SongLibrary
         }
 
         private void Song_Library_Add_Song_To_Playlist_Click(object sender, RoutedEventArgs e) {
-            foreach(SongData song in Song_Library_List_View.SelectedItems) {
+            foreach (SongData song in Song_Library_List_View.SelectedItems) {
                 playlist.addSong(song);
             }
         }
 
         private void Song_Library_Remove_Click(object sender, RoutedEventArgs e) {
             List<string> list = new List<string>();
-            foreach(SongData song in Song_Library_List_View.SelectedItems) {
+            foreach (SongData song in Song_Library_List_View.SelectedItems) {
                 list.Add(song.filePath);
             }
 
-            foreach(string filePath in list) {
+            foreach (string filePath in list) {
                 songLibrary.removeSong(filePath);
             }
-            
+
+        }
+
+        private void Song_Library_List_View_Click(object sender, RoutedEventArgs e) {
+            GridViewColumnHeader headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection direction;
+
+            if (headerClicked != null) {
+                if (headerClicked.Role != GridViewColumnHeaderRole.Padding) {
+                    if (headerClicked != _lastHeaderClicked) {
+                        direction = ListSortDirection.Ascending;
+                    } else {
+                        if (_lastDirection == ListSortDirection.Ascending) {
+                            direction = ListSortDirection.Descending;
+                        } else {
+                            direction = ListSortDirection.Ascending;
+                        }
+                    }
+
+                    string header = headerClicked.Column.Header as string;
+                    Sort(header, direction);
+
+                    _lastHeaderClicked = headerClicked;
+                    _lastDirection = direction;
+                }
+            }
+        }
+
+        private void Sort(string sortBy, ListSortDirection direction) {
+            ICollectionView dataView = CollectionViewSource.GetDefaultView(Song_Library_List_View.ItemsSource);
+
+            dataView.SortDescriptions.Clear();
+            SortDescription sd = new SortDescription(sortBy, direction);
+            dataView.SortDescriptions.Add(sd);
+            dataView.Refresh();
         }
     }
 }
